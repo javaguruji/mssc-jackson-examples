4faecf74779281fdccefa8f0c4928b7b3653ea84
Badrikant Soni <badrikant.soni@inmobi.com>
2019-09-14 20:19:05 +0530

    first commit

---
+++ src/test/java/guru/springfw/msscjacksonexamples/MsscJacksonExamplesApplicationTests.java
@@ -0,0 +1,15 @@
+package guru.springfw.msscjacksonexamples;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class MsscJacksonExamplesApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
