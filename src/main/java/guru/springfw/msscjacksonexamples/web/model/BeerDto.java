98824fac9d4b5fca8cb1387dd4862a8ed4d90059
Badrikant Soni <badrikant.soni@inmobi.com>
2019-09-14 21:06:36 +0530

    adding json format example

--- src/main/java/guru/springfw/msscjacksonexamples/web/model/BeerDto.java
+++ src/main/java/guru/springfw/msscjacksonexamples/web/model/BeerDto.java
@@ -1,5 +1,6 @@
 package guru.springfw.msscjacksonexamples.web.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -35,7 +36,9 @@ public class BeerDto {
     @Positive
     private Long upc;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
     private BigDecimal price;
+    @JsonFormat(pattern = "yyyy-MM-dd", shape = JsonFormat.Shape.STRING)
     private OffsetDateTime createdDate;
     private OffsetDateTime lastUpdatedDate;
 }
